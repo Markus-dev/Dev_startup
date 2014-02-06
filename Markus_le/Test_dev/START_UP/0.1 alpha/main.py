3a914ee56e46b6e91706513251d02115453114b8
Markus-macbook <troslolo@gmail.com>
2014-02-07 07:08:25 +1100

    Создание опоры
    Создание опоры на основе install. Папку и загрузку файлов из каталога
    пока не создаёт. Версия alpha 0.1

---
+++ Markus_le/Test_dev/START_UP/0.1 alpha/main.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import os
+os.system('clear')
+print 'START_UP core'
+import time
+time.sleep(1)
+version = '0.1 test alpha'
+version_startup = 'mkfir_5 dev inc v.alpha ; python Python 2.7.5 LLVM 5.0'
+name = raw_input("What is your name? ")
+print "Hello there, " + name + "!"
+import time
+time.sleep(1)
+pass
+print 'Program started now'
+import time
+time.sleep(1)
+print 'Version:'
+print version
+print 'Startup version:'
+print version_startup
+import time
+time.sleep(1)
+print 'Start'
+time.sleep(1)
+print '[----------------STARTING----------------]'
+import time
+time.sleep(5)
+pass
+
+import time
+import sys
+
+toolbar_width = 40
+
+# setup toolbar
+sys.stdout.write("[%s]" % (" " * toolbar_width))
+sys.stdout.flush()
+sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+
+for i in xrange(toolbar_width):
+    time.sleep(0.1) # do real work here
+    # update the bar
+    sys.stdout.write("-")
+    sys.stdout.flush()
+
+sys.stdout.write("\n")
+
+pass
+time.sleep(0.5)
+print 'loading is successful'
+print 'turn on loader'
+time.sleep(0.5)
+print 'get cwd'
+time.sleep(1)
+print os.getcwd()
+print 'create floder'
+time.sleep(0.5)
+
+#dir create
+
+pass
+
+# make error
+pass
+os.system('clear')
+print 'ERROR! PROGRAM IS NOT DONE!'
+print 'https://github.com/Markus-dev/Dev_startup/tree/master/Markus_le/Test_dev/START_UP'
+print 'Wait for relise'
+cn = raw_input("Connect? ")
+pass
+# end error
+print 'connect is not done ERROR'
+
+
+
